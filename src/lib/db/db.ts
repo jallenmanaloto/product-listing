1fe6f258b07f754917597f7255817dea8a4ff590
jallenmanaloto <jallen.manaloto@gmail.com>
2024-08-10 12:14:25 +0800

    feat: create instance of pg database connection

---
+++ src/lib/db/db.ts
@@ -0,0 +1,21 @@
+import { Pool } from 'pg';
+
+let pool: Pool | null = null;
+
+function createPool() {
+  return new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+  });
+}
+
+export function getPool() {
+  if (!pool) {
+    pool = createPool();
+  }
+
+  return pool;
+}
