986f7fc4d15d9b96d907ea128786d3b5a1a876ea
jallenmanaloto <jallen.manaloto@gmail.com>
2024-08-10 12:33:59 +0800

    feat: add conditional ssl for dev env

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -3,12 +3,17 @@ import { Pool } from 'pg';
 let pool: Pool | null = null;
 
 function createPool() {
+  const isDevelopment = process.env.NODE_ENV === 'development';
+
   return new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: parseInt(process.env.DB_PORT || '5432', 10),
+    ssl: isDevelopment && process.env.DB_SSL === 'true' ? {
+      rejectUnauthorized: false // Allow self-signed certificates in development
+    } : undefined // Use default settings for other environments
   });
 }
 
